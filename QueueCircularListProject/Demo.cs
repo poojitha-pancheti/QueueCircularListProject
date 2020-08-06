424ea1afbfd35197b574fe54dad2fa2dcd212a4a
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-06 17:34:46 +0530

    Add project files.

---
+++ QueueCircularListProject/Demo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace QueueCircularListProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int choice, x;
+            QueueCL qu = new QueueCL();
+            while (true)
+            {
+                Console.WriteLine("1.Insert an element in the queue");
+                Console.WriteLine("2.Delete an element from the queue");
+                Console.WriteLine("3.Display an element at the front");
+                Console.WriteLine("4.Display all the elements from the queue");
+                Console.WriteLine("5.Display the size of queue");
+                Console.WriteLine("6.Quit");
+                Console.WriteLine("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 6)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter element to be inserted:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        qu.Insert(x);
+                        break;
+                    case 2:
+                        x = qu.Delete();
+                        Console.WriteLine("Element deleted:" + x);
+                        break;
+                    case 3:
+                        Console.WriteLine("Element at the front:" + qu.Peek());
+                        break;
+                    case 4:
+                        qu.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Size of the queue:" + qu.size());
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ QueueCircularListProject/QueueCL.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueCircularListProject
+{
+    class QueueCL
+    {
+        private Node rear;
+        public QueueCL()
+        {
+            rear = null;
+        }
+        public bool IsEmpty()
+        {
+            return (rear == null);
+        }
+        public void Insert(int x)
+        {
+            Node temp = new Node(x);
+            if (IsEmpty())
+            {
+                rear = temp;
+                rear.link = rear;
+
+            }
+            else
+            {
+                temp.link = rear.link;
+                rear.link = temp;
+                rear = temp;
+            }
+        }
+        public int Delete()
+        {
+            if (IsEmpty())
+                throw new System.InvalidOperationException("Queue underflow");
+            Node temp;
+            if (rear.link == rear)
+            {
+                temp = rear;
+                rear = null;
+            }
+            else
+            {
+                temp = rear.link;
+                rear.link = temp.link;
+            }
+            return temp.info;
+        }
+        public int Peek()
+        {
+            if(IsEmpty())
+            throw new System.InvalidOperationException("Queue underflow");
+            return rear.link.info;
+        }
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            Console.WriteLine("Queue is:");
+            Node p = rear.link;
+            do
+            {
+                Console.WriteLine(p.info + "");
+                p = p.link;
+            } while (p != rear.link);
+            Console.WriteLine();
+        }
+        public int size()
+        {
+            if (IsEmpty())
+                return 0;
+            int s = 0;
+            Node p = rear.link;
+            do
+            {
+                s++;
+                p = p.link;
+            } while (p != rear.link);
+            return s;
+        }
+    }
+}
